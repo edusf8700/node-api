6f5f74e914d2adad59693267b85ac52b76059ac7
Eduardo Freitas <edusf8700@gmail.com>
2019-09-19 02:36:14 -0300

    Criando controles e rotas

--- server.js
+++ server.js
@@ -7,15 +7,7 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/nodeapi');
 
 requireDir('./src/models');
-const Product = mongoose.model('Product');
 
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Built native apps with react',
-        url: 'http://github.com/facebook/react-native',
-    });
-    res.send('Hello');
-});
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
